0f389bf86b55efa8c86821af986e9f8126b99278
rongjiayu <rongjiayu@gmail.com>
2023-07-13 16:41:03 +0800

    gogogo

---
+++ README.md
@@ -0,0 +1,4 @@
+
+用于工厂线上生产测试的工具，跑在x86工控机上，用JSON-RPC+HTTP/UART与设备、IO扩展板通讯
+没写过dart，chatgpt做的玩具项目，存档...
+卡在了实现HTTP和UART的StreamChannel<String>的地方，等着什么时候捞起来写写..
--- production_mate/lib/main.dart
+++ production_mate/lib/main.dart
@@ -1,4 +1,149 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:json_rpc_2/json_rpc_2.dart';
+import 'package:json_rpc_2/error_code.dart' as error_code;
+import 'package:libserialport/libserialport.dart';
+import 'SerialPortStreamChannel.dart'
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+class TestPage extends StatefulWidget {
+  const TestPage({super.key});
+
+  @override
+  _TestPageState createState() => _TestPageState();
+}
+
+class _TestPageState extends State<TestPage> {
+  List<TestItem> _testItems = [
+    TestItem("Test JSON-RPC over HTTP", (httpClient, _) async {
+      final httpResult = await httpClient.sendRequest('echo', ['hello']);
+      if (httpResult != 'hello') {
+        throw Exception('Test failed');
+      }
+    }, false),
+    TestItem("Test JSON-RPC over SerialPort", (_, serialPortClient) async {
+      final serialPortResult = await serialPortClient.sendRequest('echo', ['world']);
+      if (serialPortResult != 'world') {
+        throw Exception('Test failed');
+      }
+    }, false),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Test Page"),
+      ),
+      body: Column(
+        children: [
+          DataTable(
+            columns: [
+              DataColumn(label: Text("Test Item")),
+              DataColumn(label: Text("Result")),
+            ],
+            rows: _testItems.map((item) {
+              return DataRow(cells: [
+                DataCell(Text(item.name)),
+                DataCell(Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  decoration: BoxDecoration(
+                    color: item.result ? Colors.green : Colors.red,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Text(
+                    item.result ? "Pass" : "Fail",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )),
+              ]);
+            }).toList(),
+          ),
+          ElevatedButton(
+            child: Text("Start Test"),
+            onPressed: () {
+              _runTests();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+
+  void _runTests() {
+    for (final item in _testItems) {
+      _runTest(item);
+    }
+  }
+
+  void _runTest(TestItem item) {
+    bool isCancelled = false;
+
+    final timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      if (isCancelled) {
+        timer.cancel();
+      }
+    });
+
+    // TODO: Implement the test logic here
+
+    timer.cancel();
+
+    item.result = true; // For testing purpose only
+    setState(() {});
+  }
+}
+
+class TestItem {
+  final String name;
+  final Function(httpClient, serialPortClient) testFunction;
+  bool result;
+
+  TestItem(this.name, this.testFunction, this.result);
+
+  Future<void> runTest() async {
+    // Create a HTTP client
+    var socket = WebSocketChannel.connect(Uri.parse('ws://localhost:4321'));
+    final httpClient = Client(socket);
+
+    // Create a SerialPort client
+
+    final client = Client(socket.cast<String>());
+    // final serialPortClient = SerialPortClient('/dev/ttyUSB0');
+
+    // Connect to both clients
+    await Future.wait([
+      httpClient.connect(Uri.parse('http://localhost:8080')),
+      serialPortClient.connect(),
+    ]);
+
+    try {
+      // Run the test function
+      await testFunction(httpClient, serialPortClient);
+
+      result = true;
+    } on RpcException catch (e) {
+      if (e.code == error_code.methodNotFound) {
+        // Handle method not found error
+        result = false;
+      } else {
+        // Handle other errors
+        result = false;
+      }
+    } finally {
+      // Close both connections
+      await Future.wait([
+        httpClient.close(),
+        serialPortClient.close(),
+      ]);
+    }
+  }
+}
+
 
 void main() {
   runApp(const MyApp());
@@ -31,95 +176,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TestPage(),
     );
   }
 }
